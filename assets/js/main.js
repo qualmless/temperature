ef04114772e94322940588c5763e48573345efdc
Tara Dunmore <30328414+qualmless@users.noreply.github.com>
2020-12-02 07:52:04 -0600

    removed old comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,12 @@ function whichConvert(){
     if (fahrenheitRadio){
         let farenheit = document.getElementById("inputValue").value;
         document.getElementById("finalValue").innerHTML = ((farenheit - 32) * (5/9));
-        // let degree = document.getElementsByName("degree");
         let degree = document.getElementById("fahrenheitRadio").checked;
         console.log((farenheit - 32) * (5/9));
     }
     else if (celsiusRadio) {
         let celsius = document.getElementById("inputValue").value;
         document.getElementById("finalValue").innerHTML = ((celsius * (9/5)) + 32);
-        // let degree = document.getElementsByName("degree");
         let degree = document.getElementById("celsiusRadio").checked;
         console.log((celsius * (9/5)) + 32);
     }
@@ -21,17 +19,4 @@ function whichConvert(){
     }
 }
 
-// function fToCelsius(){
-//     let farenheit = document.getElementById("inputValue").value;
-//     document.getElementById("finalValue").innerHTML = (((32*farenheit) - 32) * (5/9));
-//     // let degree = document.getElementsByName("degree");
-//     let degree = document.getElementById("fahrenheitRadio").checked;
-//     console.log(degree)
-//     console.log(((32*farenheit) - 32) * (5/9));
-//     // return (((32*farenheit) - 32) * (5/9));
-// }
-
-// function cToFarenheit(){
-
-// }
 
