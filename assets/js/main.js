865a21d832ad2a9088e1db51fa77b67b587149c1
Tara Dunmore <30328414+qualmless@users.noreply.github.com>
2020-12-02 16:34:01 -0600

    clarified code with comments, updated function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,24 @@
 function whichConvert(){
+
+    // pulls in data type radio buttons
     let fahrenheitRadio = document.getElementById("fahrenheitRadio").checked;
     let celsiusRadio = document.getElementById("celsiusRadio").checked;
 
+    // converts to fahrenheit if selected
     if (fahrenheitRadio){
         let farenheit = document.getElementById("inputValue").value;
         document.getElementById("finalValue").value = ((farenheit - 32) * (5/9));
-        let degree = document.getElementById("fahrenheitRadio").checked;
-        console.log((farenheit - 32) * (5/9));
+        // let degree = document.getElementById("fahrenheitRadio").checked;
     }
+
+    // converts to celsius if selected
     else if (celsiusRadio) {
         let celsius = document.getElementById("inputValue").value;
         document.getElementById("finalValue").value = ((celsius * (9/5)) + 32);
-        let degree = document.getElementById("celsiusRadio").checked;
-        console.log((celsius * (9/5)) + 32);
+        // let degree = document.getElementById("celsiusRadio").checked;
     }
+
+    // error message if no button selected
     else{
         document.getElementById("errorMessage").innerHTML="Please pick a conversion type."
     }
