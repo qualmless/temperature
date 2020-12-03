9948494c8ba43e5c22bfc0c25dd64f88e8cbeb07
Tara Dunmore <30328414+qualmless@users.noreply.github.com>
2020-12-03 11:15:56 -0600

    radio button iterator working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 function whichConvert(){
 
     // pulls in data type radio buttons
-    let fahrenheitRadio = document.getElementById("fahrenheitRadio").checked;
-    let celsiusRadio = document.getElementById("celsiusRadio").checked;
+    // let fahrenheitRadio = document.getElementById("fahrenheitRadio").checked;
+    // let celsiusRadio = document.getElementById("celsiusRadio").checked;
+    
+    // pulls in radio button values
+    const unitType = document.querySelectorAll('input[name="unit"]');
+
+    // iterates over radio buttons
+    let selectedValue;
+    for (const rb of unitType) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            break;
+        }
+    }
 
     // converts to fahrenheit if selected
-    if (fahrenheitRadio){
+    if (selectedValue=="fahrRadio"){
         let fahrenheit = document.getElementById("inputValue").value;
         let celsius = Math.round((fahrenheit - 32) * (5/9))
         document.getElementById("newValue").innerHTML = celsius;
@@ -18,7 +30,7 @@ function whichConvert(){
     }
 
     // converts to celsius if selected
-    else if (celsiusRadio) {
+    else if (selectedValue=="celsRadio") {
         let celsius = document.getElementById("inputValue").value;
         let fahrenheit = Math.round((celsius * (9/5)) + 32)
         document.getElementById("newValue").innerHTML = fahrenheit;
@@ -35,6 +47,7 @@ function whichConvert(){
     else{
         document.getElementById("errorMessage").innerHTML="Please pick a conversion type."
     }
+
 }
 
 
