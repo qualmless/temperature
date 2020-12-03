1c9b1a6f9dcaa9a42298807ac13d7dbfbc2f4a09
Tara Dunmore <30328414+qualmless@users.noreply.github.com>
2020-12-03 11:00:27 -0600

    added round to integer, spellchecked fahrenheit, added styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,29 @@ function whichConvert(){
 
     // converts to fahrenheit if selected
     if (fahrenheitRadio){
-        let farenheit = document.getElementById("inputValue").value;
-        document.getElementById("finalValue").value = ((farenheit - 32) * (5/9));
-        // let degree = document.getElementById("fahrenheitRadio").checked;
+        let fahrenheit = document.getElementById("inputValue").value;
+        let celsius = Math.round((fahrenheit - 32) * (5/9))
+        document.getElementById("newValue").innerHTML = celsius;
+        document.getElementById("newUnit").innerHTML = "Celsius"
+        document.getElementById("originalValue").innerHTML = fahrenheit;
+        document.getElementById("originalUnit").innerHTML="Fahrenheit";
+
+        // clears error if created before click
+        document.getElementById("errorMessage").innerHTML=""
     }
 
     // converts to celsius if selected
     else if (celsiusRadio) {
         let celsius = document.getElementById("inputValue").value;
-        document.getElementById("finalValue").value = ((celsius * (9/5)) + 32);
-        // let degree = document.getElementById("celsiusRadio").checked;
+        let fahrenheit = Math.round((celsius * (9/5)) + 32)
+        document.getElementById("newValue").innerHTML = fahrenheit;
+        document.getElementById("newUnit").innerHTML = "Fahrenheit";
+        document.getElementById("originalValue").innerHTML = celsius;
+        document.getElementById("originalUnit").innerHTML = "Celsius";
+
+        // clears error if created before click
+        document.getElementById("errorMessage").innerHTML=""
+
     }
 
     // error message if no button selected
